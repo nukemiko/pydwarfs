5af3b497e07ec5208dd0a8c244de8668ecf6df51
Nukemiko <53572814+nukemiko@users.noreply.github.com>
2024-01-05 14:25:04 +0800

    Improved parsing of /proc/mounts

--- src/pydwarfs/utils.py
+++ src/pydwarfs/utils.py
@@ -27,16 +27,22 @@ class MountPoint:
 
     @classmethod
     def parse(cls, line: str) -> Self:
-        line_parts = line.rstrip().split()
+        line_parts = [s.replace('\\040', ' ').replace('\\012', '\n') for s in line.strip(' \n').split(' ')]
         if len(line_parts) != 6:
-            raise ValueError('')
+            raise ValueError(f'Not a valid line of mount info: {line!r}')
 
         fs_spec = line_parts[0]
         fs_file = line_parts[1]
         fs_vfstype = line_parts[2]
         fs_mntops = tuple(line_parts[3].split(','))
-        fs_freq = int(line_parts[4])
-        fs_passno = int(line_parts[5])
+        raw_fs_freq = line_parts[4]
+        if not raw_fs_freq.isdigit():
+            raise ValueError(f'Invalid value of fs_freq: {raw_fs_freq!r}')
+        fs_freq = int(raw_fs_freq)
+        raw_fs_passno = line_parts[5]
+        if not raw_fs_passno.isdigit():
+            raise ValueError(f'Invalid value of fs_passno: {raw_fs_passno!r}')
+        fs_passno = int(raw_fs_passno)
 
         target = Path(str(bytes(fs_file, encoding='raw_unicode_escape'), encoding='unicode_escape'))
 
@@ -44,8 +50,10 @@ class MountPoint:
 
 
 def _iter_get_all_mounts_points() -> Iterator[MountPoint]:
-    with open('/proc/mounts', mode='r') as f:
-        for line in f:
+    with open('/proc/mounts', mode='r', newline='') as f:
+        mount_info_content = f.read()
+    for line in mount_info_content.split('\n'):
+        if line.strip():
             yield MountPoint.parse(line)
 
 
