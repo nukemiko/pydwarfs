51f9b445bc69a3d04155729b48fd51150f0ca83e
Nukemiko <53572814+nukemiko@users.noreply.github.com>
2024-01-06 22:21:09 +0800

    Modified the encoding declaration for every .py files

--- src/pydwarfs/__init__.py
+++ src/pydwarfs/__init__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 import os
 
 from packaging.version import Version
--- src/pydwarfs/utils.py
+++ src/pydwarfs/utils.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 import errno
 import os
 from pathlib import PosixPath as Path
